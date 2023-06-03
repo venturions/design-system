29e29d7640c39ddb5226ea891c17763cb6602bdf
Alex Costa <alex.costa@bitfy.app>
2023-06-03 15:33:05 -0300

    criação da estrutura do design system e storybooks

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { styled } from "./styles";
+
+const Button = styled("button", {
+  fontFamily: "$default",
+  backgroundColor: "$ignite500",
+  borderRadius: "$md",
+  padding: "$4",
+});
+
+export function App() {
+  return <Button>Hello World</Button>;
+}
