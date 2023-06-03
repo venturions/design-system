059133574c61aa915cf9a1d9117da42569399b7e
Alex Costa <alex.costa@bitfy.app>
2023-06-03 16:12:49 -0300

    criação do button primary e secondary

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,13 +1,34 @@
-/* eslint-disable prettier/prettier */
-import { styled } from "./styles";
+import { ComponentProps } from 'react'
+import { styled } from './styles'
 
-const Button = styled("button", {
-  fontFamily: "$default",
-  backgroundColor: "$ignite500",
-  borderRadius: "$md",
-  padding: "$4",
-});
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: '$bold',
+  color: '$white',
 
-export function App() {
-  return <Button>Hello World</Button>;
-}
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+
+  '&:hover': {
+    backgroundColor: '$ignite300',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
