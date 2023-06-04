cbead49a0aa7bf43a77f786c373898cce59f157e
Alex Costa <alex.costa@bitfy.app>
2023-06-03 21:09:03 -0300

    componente de heading e adição de docs no storybook

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -4,6 +4,7 @@ import { Text, TextProps } from '@ignite-ui/react'
 export default {
   title: 'Typography/Text',
   component: Text,
+  tags: ['autodocs'],
   args: {
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum cumque explicabo reiciendis nemo, totam dolor dolores, animi rem illo aperiam sit quidem! Deserunt alias itaque repellendus facilis accusantium ad accusamus!',
